fd68247e1080947c3f6af2d69aedbe13c03bfed7
dan <79697363+DaniMinn@users.noreply.github.com>
2021-12-14 11:21:18 +0100

    chiamata al server pre prendere i dati dal json

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-/* var appStatus = []
-axios.get('database.json').then(function (risultato) {  //definisco una funzione in cui salvo il risultato della chiamata
-    console.log(risultato.data.appStatus);
-    appStatus = risultato.data.appStatus;
-    
-});
 
-console.log(appStatus); */
+//definisco array per salvare i dati del json, è importante definirlo prima di fare la chiamata
+var data = [];
 
 
-
-var appStatus =
-    [
-        {
-            "app": "Application A",
-            "category": "Overall Status",
-            "status": "1"
-        },
-        {
-            "app": "Application A",
-            "category": "System Status",
-            "status": "2"
-        },
-        {
-            "app": "Application B",
-            "category": "Overall Status",
-            "status": "1"
-        },
-        {
-            "app": "Application B",
-            "category": "Overall Status",
-            "status": "2"
+//faccio la chiamata al server 
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    // è importante che la chiamata sia sincrona, FALSE, xk se la faccio asincrona (TRUE) lo script viene eseguito prima
+    // di prendere i dati del file json è quidi non posso riempire le carte
+    rawFile.open("GET", file, false);
+    //controllo che il server risponde
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
         }
-    ]
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("database.json", function (text) {
+    data = JSON.parse(text);
+    console.log(data);    
+});
 
 
 function renderCards() {
 
     
-    for (var i = 0; i < appStatus.length; i++) {
+    for (var i = 0; i < data.appStatus.length; i++) {
         
         //creo i contenitori delle carte
         var div = document.createElement('div');
@@ -49,30 +41,38 @@ function renderCards() {
         /* metto il titolo */
         var div = document.createElement('div');
         div.className = 'card-title';
-        div.innerHTML = appStatus[i].app;
+        div.innerHTML = data.appStatus[i].app;
         document.getElementsByClassName('card-body')[i].appendChild(div);
 
         /* metto il pallino per indicare lo status sul titolo */
         let status = document.createElement('span');
         status.className = 'status';
-        status.innerHTML = appStatus[i].status;
+        status.innerHTML = data.appStatus[i].status;
         document.getElementsByClassName('card-title')[i].appendChild(status);
         
+        
+        var cat = document.createElement('div');
+        cat.className = 'card-cat';
+        cat.innerHTML = data.appStatus[i].category;
+        document.getElementsByClassName('card-body')[i].appendChild(cat);
+        
+        let status2 = document.createElement('span');
+        status2.className = 'status';
+        status2.innerHTML = data.appStatus[i].status;
+        document.getElementsByClassName('card-cat')[i].appendChild(status2);
+        
+        
+        //coloro i pallini in base allo status
         let pallino = document.getElementsByClassName('status');
         for (let j = 0; j < pallino.length; j++) {
             let key = pallino[j].innerHTML;
             console.log(key);
             if (key === '1') {
-               console.log('caso1');
                 pallino[j].style.background ='#01FF00';
             } else {
-                console.log('caso2');
                 pallino[j].style.background = 'red';
             }
         }
-        
-        
-
    
 
                
